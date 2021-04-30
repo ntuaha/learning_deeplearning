c152e209b5999882683dc008fe7e37e70b57d15c
aha <ntuaha@gmail.com>
2021-05-01 00:31:55 +0800

    add numerical_diff

---
+++ tests/test_util.py
@@ -0,0 +1,17 @@
+import pytest
+# https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+from src.VARIABLE import Variable
+from src.FUNCTION import *
+from src.util import *
+import numpy as np
+
+def test_numerical_diff():
+    # P16
+    f = Square()
+    x = Variable(np.array(2))
+    y = numerical_diff(f,x)
+    check_1 = Variable((f(Variable(np.array(2+1e-4))).data - f(Variable(np.array(2-1e-4))).data) / (2 * 1e-4))
+    return check_1 == y
